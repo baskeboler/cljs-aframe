bfe5586db422219d280a7537e53a1505ab042506
Victor Gil <baskeboler@gmail.com>
2019-01-07 04:21:40 -0300

    360 video

--- src/main/cljs_aframe/app.cljs
+++ src/main/cljs_aframe/app.cljs
@@ -1,7 +1,53 @@
 (ns cljs-aframe.app
   (:require [reagent.core :as r :refer [render]]
-            ["aframe" :as aframe]))
+            ["aframe"]
+            ["aframe-mountain-component"]
+            ["aframe-sun-sky"]
+            ["aframe-particle-system-component"]))
 
+;; (comment
+  ;; "    <a-scene>
+
+      ;; <a-entity position="0 2.25 -15" particle-system="color: #000,#FFF"></a-entity>
+
+      ;; <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+      ;; <a-box position="-1 0.5 -3" rotation="0 45 0" width="1" height="1" depth="1"  color="#4CC3D9"></a-box>
+      ;; <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+      ;; <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+
+      ;; <a-sky color="#000000"></a-sky>
+
+    ;; </a-scene>")
+
+(defn stars-scene []
+  [:a-scene
+   [:a-entity
+    {:position "0 2.25 -15"
+     :particle-system "preset: rain; color: #000,#FFF"}]
+   [:a-sphere
+    {:position "0 1.25 -5"
+     :radius "1.25"
+     :color "#EF2D5E"}]
+   [:a-box
+    {:position "-1 0.5 -3"
+     :rotation "0 45 0"
+     :width 1
+     :height 1
+     :depth 1
+     :color "#4CC3D9"}]
+   [:a-cylinder
+    {:position "1 0.75 -3"
+     :radius 0.5
+     :height 1.5
+     :color "#FFC65D"}]
+   [:a-plane
+    {:position "0 0 -4"
+     :rotation "-90 0 0"
+     :width 4
+     :height 4
+     :color "#7BC8A4"}]
+   [:a-sky
+    {:color "#000000"}]])
 
 (defn box []
   (let [seconds-elapsed (r/atom 0)]
@@ -27,25 +73,49 @@
   [:a-camera
    [:a-cursor]])
 
+(defn painting-panorama []
+  [:a-sky
+   {:src "/images/panorama.jpeg"}])
+
+(defn assets []
+  [:a-assets
+   [:a-asset-item {:id "castle"
+                   :src "/models/scene.gltf"}]])
+
+(defn video []
+  [:video
+   {:src "http://raspberrypi:8090"
+    :controls true
+    :auto-play true
+    :width "100%"}])
+
+(defn video-sphere []
+  [:a-videosphere {:src "videos/video.mp4"
+                   :autoplay true
+                   :loop true}])
+
 (defn scene []
   [:a-scene {:stats true}
-   [box]
-   [sphere]
-   [cylinder]
-   [plane]
-   [sky]
-   [player]])
+   ;; [assets]
+   ;; [painting-panorama]
+    [video-sphere]
+   ])
+   ;; [box]
+   ;; [:a-sun-sky {:material "side: back;"}]
+   ;; [:a-mountain {:color "#222222"}]])
+   ;; [:a-gltf-model {:src "#castle"}]])
+   ;; [sphere]
+   ;; [cylinder]
+   ;; [plane]
+   ;; [sky]
+   ;; [player]
+   
 
 
-(defn app []
-  [:div.app
-   [:h1 "hello world!"]
-   [:hr]
-   [scene]])
 
 (defn mount-components []
   (render
-    [app]
+    [scene]
     (. js/document (getElementById "root"))))
 
 (defn init! []
